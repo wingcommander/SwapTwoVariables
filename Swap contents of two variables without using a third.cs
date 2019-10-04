6092d885999184888fa67d18ac6ed72c1e280058
wingcommander <garvanjones@gmail.com>
2019-10-04 11:37:19 +0100

    no message

---
+++ Swap contents of two variables without using a third.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swap_contents_of_two_variables_without_a_third
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 4;
+            int b = 7;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            Console.ReadLine();
+        }
+    }
+}
